e9d79874da178b346fc7409614f9db0493915114
pav9551 <pav9551@yandex.ru>
2025-02-10 23:04:37 +0300

    added fixes

--- src/masks.py
+++ src/masks.py
@@ -1,8 +1,11 @@
 def get_mask_card_number(card_number: str) -> str:
-    """Функция, которая возвращает маску номера карты"""
-    return f"{card_number[:4]} {card_number[6:8]}** **** {card_number[12:]}"
+    """Функция, которая возвращает маску номера карты."""
+    if len(card_number) != 16:
+        raise ValueError("Неверная длина номера карты.")
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
 def get_mask_account(account_number: str) -> str:
-    """Функция, которая возвращает маску номера счета"""
-    #return f"{'*' * 2}{bank_account[-4:]}"
+    """Функция, которая возвращает маску номера счета."""
+    if not (8 <= len(account_number) <= 16):
+        raise ValueError("Неверная длина номера счета.")
     return f"{'*' * (len(account_number) - 4)}{account_number[-4:]}"
